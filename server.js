957988d35de9ddad1259cf91031a58fdd525cd47
Adel Senhadji <adelsenhadjii@gmail.com>
2020-09-18 12:08:03 +0200

    init postgres connection

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ app.use(express.json())
 app.use(require('./auth'))
 
 
+const { Client } = require('pg')
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'hermedb',
+    password: 'password',
+})
+client.connect()
+client.query('SELECT NOW()', (err, res) => {
+    console.log("OK")
+    client.end()
+})
+
 app.get('/', (req, res) => {
     res.send('ok')
 })
