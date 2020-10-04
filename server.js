548327f7d190a8c9ae402c3117f6c277962f230f
Adel Senhadji <adelsenhadjii@gmail.com>
2020-10-04 13:55:31 +0200

    add more conditions and error handling for friends requests

--- server.js
+++ server.js
@@ -46,18 +46,21 @@ app.get('/users/:id', auth.authToken, async (req, res) => {
         res.status(400).send()
         return
     }
+    //-----------------------------
+    // const userExistsQuery = `SELECT * FROM usrs WHERE username='${userId}'`
+    // let userExists = new Promise((resolve, reject) => {
+    //     client.query(userExistsQuery, (err, result) => {
+    //         if (err) throw err;
+    //         if (!result.rows.length)
+    //             resolve(false)
+    //         resolve(true)
+    //     })
+    // });
 
-    const userExistsQuery = `SELECT * FROM usrs WHERE username='${userId}'`
-    let userExists = new Promise((resolve, reject) => {
-        client.query(userExistsQuery, (err, result) => {
-            if (err) throw err;
-            if (!result.rows.length)
-                resolve(false)
-            resolve(true)
-        })
-    });
+    // userExists = await userExists
+    // ----------------------------------------------
 
-    userExists = await userExists
+    let userExists = await dataExist('usrs', 'username', userId)
 
     if (!userExists) {
         res.status(404).send()
@@ -120,15 +123,16 @@ app.post('/posts', auth.authToken, async (req, res) => {
 
     do {
         id = Math.floor(Math.random() * (9999 - 1000) + 1000)
-        let idExists = new Promise((resolve, reject) => {
-            client.query(checkIdQuery + `'${id}'`, (err, result) => {
-                if (err) throw err;
-                if (!result.rows.length)
-                    resolve(false)
-                resolve(true)
-            })
-        });
-        idExists = await idExists
+        // let idExists = new Promise((resolve, reject) => {
+        //     client.query(checkIdQuery + `'${id}'`, (err, result) => {
+        //         if (err) throw err;
+        //         if (!result.rows.length)
+        //             resolve(false)
+        //         resolve(true)
+        //     })
+        // });
+        // idExists = await idExists
+        idExists = await dataExist('posts', 'id_post', id)
     } while (idExists);
 
     const post = {
@@ -150,17 +154,19 @@ app.post('/posts', auth.authToken, async (req, res) => {
 app.delete('/posts', auth.authToken, async (req, res) => {
     const username = req.username
     const id = req.body.id
-    const postExistsQuery = `SELECT * FROM posts WHERE id_post='${id}'`
 
-    let postExists = new Promise((resolve, reject) => {
-        client.query(postExistsQuery, (err, result) => {
-            if (err) throw err;
-            if (!result.rows.length)
-                resolve(false)
-            resolve(true)
-        })
-    });
-    postExists = await postExists
+    // const postExistsQuery = `SELECT * FROM posts WHERE id_post='${id}'`
+    // let postExists = new Promise((resolve, reject) => {
+    //     client.query(postExistsQuery, (err, result) => {
+    //         if (err) throw err;
+    //         if (!result.rows.length)
+    //             resolve(false)
+    //         resolve(true)
+    //     })
+    // });
+    // postExists = await postExists
+
+    let postExists = await dataExist('posts', 'id_post', id)
 
     if (!postExists) {
         res.status(404).send()
@@ -181,27 +187,24 @@ app.get('/friends', auth.authToken, async (req, res) => {
     let friends = new Promise((resolve, reject) => {
         client.query(friendsQuery, (err, results) => {
             if (err) throw err;
-            if (!results.rows.length)
-                resolve(false)
             resolve(results.rows)
         })
     });
 
     friends = await friends
 
-    console.log(friends);
     if (!friends.length) {
         res.status(200).json({ friends: [] })
         return
     }
 
-    let queryInValues = '('
+    let queryInValues = '(';
     friends.forEach(f => {
         queryInValues += `'${f.friend}',`
     });
     queryInValues = queryInValues.slice(0, -1);
 
-    const getFriendsQuery = `SELECT username,first_name,last_name FROM usrs WHERE username IN ${queryInValues}) `
+    const getFriendsQuery = `SELECT username,first_name,last_name FROM usrs WHERE username IN ${queryInValues})`
     client.query(getFriendsQuery, (err, results) => {
         if (err) throw err;
         res.status(200).json(results.rows)
@@ -217,7 +220,7 @@ app.get('/requests', auth.authToken, (req, res) => {
     })
 })
 
-app.post('/requests/add', auth.authToken, async (req, res) => {
+app.post('/requests/invite', auth.authToken, async (req, res) => {
     const username = req.username
     const friend_username = req.body.friend
 
@@ -226,23 +229,42 @@ app.post('/requests/add', auth.authToken, async (req, res) => {
         return
     }
 
+    // const checkFriendQuery = `SELECT * FROM usrs WHERE username='${friend_username}'`
+    // let friendExists = new Promise((resolve, reject) => {
+    //     client.query(checkFriendQuery, (err, result) => {
+    //         if (err) throw err;
+    //         if (!result.rows.length)
+    //             resolve(false)
+    //         resolve(true)
+    //     })
+    // });
 
+    // friendExists = await friendExists
 
-    const checkFriendQuery = `SELECT * FROM usrs WHERE username='${friend_username}'`
-
-    let friendExists = new Promise((resolve, reject) => {
-        client.query(checkFriendQuery, (err, result) => {
-            if (err) throw err;
-            if (!result.rows.length)
-                resolve(false)
-            resolve(true)
-        })
-    });
-
-    friendExists = await friendExists
+    let friendExists = await dataExist('usrs', 'username', friend_username)
 
     if (!friendExists) {
         res.status(404).send('not found')
+        return
+    }
+
+    let alreadyFriends = await dataPairExists('friends', 'usr', username, 'friend', friend_username)
+
+    if (alreadyFriends) {
+        res.status(400).send('alreadyFriends')
+        return
+    }
+
+    let alreadyRequested = await dataPairExists('friend_requests', 'usr', username, 'friend', friend_username)
+    let alreadyBeenInvited = await dataPairExists('friend_requests', 'usr', friend_username, 'friend', username)
+
+    if (alreadyRequested) {
+        res.status(400).send('Already requested')
+        return
+    }
+
+    if (alreadyBeenInvited) {
+        res.status(400).send('You have already been invited')
         return
     }
 
@@ -251,7 +273,6 @@ app.post('/requests/add', auth.authToken, async (req, res) => {
     client.query(requestQuery, (err, result) => {
         if (err) throw err;
         res.status(201).send()
-
     })
 })
 
@@ -264,30 +285,38 @@ app.post('/requests/accept', auth.authToken, async (req, res) => {
         return
     }
 
-    const friendExistsQuery = `SELECT usr FROM friend_requests WHERE friend='${username}' AND usr='${friendToAccept}'`
-    let friendExists = new Promise((resolve, reject) => {
-        client.query(friendExistsQuery, (err, result) => {
-            if (err) throw err;
-            if (!result.rows.length)
-                resolve(false)
-            resolve(true)
-        })
-    });
+    // const friendExistsQuery = `SELECT * FROM friend_requests WHERE friend='${username}' AND usr='${friendToAccept}'`
+    // let friendExists = new Promise((resolve, reject) => {
+    //     client.query(friendExistsQuery, (err, result) => {
+    //         if (err) throw err;
+    //         if (!result.rows.length)
+    //             resolve(false)
+    //         resolve(true)
+    //     })
+    // });
 
-    friendExists = await friendExists
+    let isUserInvited = await dataPairExists('friend_requests', 'usr', friendToAccept, 'friend', username)
 
-    if (!friendExists) {
-        res.status(404).send()
+    if (!isUserInvited) {
+        res.status(400).send()
         return
     }
 
     const addFriendQuery = `INSERT INTO friends (usr,friend)
     VALUES ('${username}','${friendToAccept}'), ('${friendToAccept}','${username}')`
-
     client.query(addFriendQuery, (err, result) => {
         if (err) throw err;
-        res.status(201).send()
+        // res.status(201).send()
     })
+
+    const removeRequestQuery = `DELETE FROM friend_requests
+    WHERE usr='${friendToAccept}' AND friend='${username}'`
+    client.query(removeRequestQuery, (err, result) => {
+        if (err) throw err;
+        res.status(200).send()
+    })
+
+
 })
 
 app.post('/register', async (req, res) => {
@@ -310,6 +339,36 @@ app.post('/register', async (req, res) => {
 
 
 
+async function dataExist(table, column, value) {
+    const query = `SELECT * FROM ${table} WHERE ${column}='${value}'`
+    let exists = new Promise((resolve, reject) => {
+        client.query(query, (err, res) => {
+            if (err) throw err
+            if (!res.rows.length)
+                resolve(false)
+            resolve(true)
+        })
+    })
+    exists = await exists
+    return exists
+}
+
+async function dataPairExists(table, column_1, value_1, column_2, value_2) {
+    const query = `SELECT * FROM ${table} WHERE ${column_1}='${value_1}' AND ${column_2}='${value_2}'`
+
+    console.log(query);
+    let exists = new Promise((resolve, reject) => {
+        client.query(query, (err, res) => {
+            if (err) throw err
+            if (!res.rows.length)
+                resolve(false)
+            resolve(true)
+        })
+    })
+    exists = await exists
+    return exists
+}
+
 
 
 /*   DANGER   ZONE    */
@@ -331,4 +390,7 @@ app.delete('/postsgenocide', (req, res) => {
 
 const port = process.env.PORT || 4000;
 console.clear()
+
+
+
 app.listen(port, () => console.log(port))
